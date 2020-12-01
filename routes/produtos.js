49930afd828f7fad18fe71171ec4b16c49d9bae7
GustavoAlvesB <gustavo.empresa1@gmail.com>
2020-11-30 23:02:18 -0300

    fializado CRUD
    craido crudo com sucesso e todas estão funcionando

--- routes/produtos.js
+++ routes/produtos.js
@@ -16,7 +16,24 @@ router.get("/",(req, res, next)=>{
             (error,resultado,field)=>{
                 //se der erro                 
                 if(error){return res.status(500).send({ error: error}); }               
-                return res.status(200).send({ response : resultado});
+                //uma forma de documentar oq o metodo faz
+                const response = {
+                    quantidade : resultado.length,
+                    produtos : resultado.map( prod =>{
+                        return {
+                            id_produto : prod.id_produto,
+                            nome: prod.nome,
+                            preco : prod.preco,
+                            request: {
+                                tipo : "GET",
+                                descricao :"Retorna todos os produtos para serem detalhados",
+                                url : "http://localhost:3000/produtos/"+prod.id_produto
+                            }
+                        }
+                    }) 
+                }
+
+                return res.status(200).send({response});
             }
         )
     })
@@ -28,13 +45,29 @@ router.get("/:id_produto",(req, res, next)=>{
         }     
         conn.query(
             'SELECT * FROM tb_produto where id_produto = ?',
-            [req.body.id_produto],
+            [req.params.id_produto],
             (error,resultado,field)=>{
-                conn.release;
+                conn.release();
                 //se der erro                 
-                if(error){return res.status(500).send({ error: error}); }               
-                return res.status(200).send({ response : resultado});
-            }
+                if(error){
+                    return res.status(500).send({ error: error}); }
+                    if (resultado == 0) {
+                        return res.status(404).send({mensagem:"Nenhum produto encontrado"})
+                    }
+                    const response = {
+                             produto :{
+                                    id_produto : resultado[0].id_produto,
+                                    nome: resultado[0].nome,
+                                    preco : resultado[0].preco,
+                                    request: {
+                                        tipo : "GET",
+                                        descricao :"Retorna todos os produto",
+                                        url : "http://localhost:3000/produtos/"
+                                    }
+                                }
+                            }
+                            return res.status(200).send({ response});
+                        }                                
         )
     })
 });
@@ -56,32 +89,71 @@ router.post("/",(req, res, next)=>{
             [req.body.nome, req.body.preco],            
             (error,resultado,field)=>{
                 //fechando conexão
-                conn.release;
+                conn.release();
                 if(error){
                     return res.status(500).send({
                         error: error,
                         response : null
                     });
                 }
-                //passando codigo da operação e mensagem alem de resumo de produto
-                res.status(201).send({
+                const response = {
                     mensagem: 'Produto inserido com sucesso',
-                    Id_produto:resultado.insertId   
-                    });
+                    produtoCriado : {
+                            id_produto : resultado.id_produto,
+                            nome: req.body.nome,
+                            preco : req.body.preco,
+                            request: {
+                                tipo : "POST",
+                                descricao :"Insere um produto",
+                                url : "http://localhost:3000/produtos/"
+                            }
+                        }                     
+                }
+                //passando codigo da operação e mensagem alem de resumo de produto
+                res.status(201).send(response);
             }
         )
     })
   
 });
 
-router.patch("/:id_produto",(req, res, next)=>{
-    res.status(201).send({
+router.patch("/",(req, res, next)=>{
+    /*res.status(201).send({
         mensagem: 'Usando rota patch de produtos.'
+    });*/
+    mysql.getConnection((error,conn)=>{   
+        if (error) {
+            return res.status(500).send({error: error})
+        }     
+        conn.query(
+            'UPDATE tb_produto SET nome = ?, preco = ? WHERE id_produto = ?',
+            [req.body.nome, req.body.preco, req.body.id_produto],
+            (error,resultado,field)=>{
+                //fechando conexão
+                conn.release();
+                if(error) {return res.status(500).send({ error: error}); }
+                const response = {
+                        mensagem: 'Produto Atualizado com sucesso',
+                        produtoAtualizado : {
+                                id_produto : req.body.id_produto,
+                                nome: req.body.nome,
+                                preco : req.body.preco,
+                                request: {
+                                    tipo : "GET",
+                                    descricao :"Retorna detalhes do produto atualizado",
+                                    url : "http://localhost:3000/produtos/" + req.body.id_produto
+                                }
+                        }                     
+                }
+                //passando codigo da operação e mensagem alem de resumo de produto
+                return res.status(202).send(response);
+            }
+        )
     });
 });
 
 
-router.delete("/:id_produto",(req, res, next)=>{
+router.delete("/",(req, res, next)=>{
    /*  testando rota
     res.status(201).send({
         mensagem: 'Usando rota delete de produtos.'
@@ -91,13 +163,27 @@ router.delete("/:id_produto",(req, res, next)=>{
             return res.status(500).send({error: error})
         }     
         conn.query(
-            'DELETE * FROM tb_produto where id_produto = ?',
+            'DELETE  FROM tb_produto WHERE id_produto = ?',
             [req.body.id_produto],
             (error,resultado,field)=>{
-                conn.release;
-                //se der erro                 
-                if(error){return res.status(500).send({ error: error}); }               
-                return res.status(200).send({ response : resultado});
+                //fechando conexão
+                conn.release();
+                if(error){
+                    return res.status(500).send({
+                        error: error,
+                        response : null
+                    });
+                }
+                const response = {
+                    mensagem: 'Produto Removido com sucesso',
+                    request: {
+                                tipo : "GET",
+                                descricao :"Retorna todos produtos",
+                                url : "http://localhost:3000/produtos/" 
+                            }
+                    }                                 
+                //passando codigo da operação e mensagem alem de resumo de produto
+                 return res.status(201).send({response});
             }
         )
     })
